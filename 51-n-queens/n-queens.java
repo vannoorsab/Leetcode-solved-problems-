59bd3e873ca241bb153cfa7fb4b923e9b398a452
Vannoorsab  .D <160221972+vannoorsab@users.noreply.github.com>
2026-08-20 17:02:36 +0530

    Time: 2 ms (81.44%) | Memory: 46.6 MB (47.07%) - LeetSync

---
+++ 51-n-queens/n-queens.java
@@ -0,0 +1,54 @@
+class Solution {
+    private void backtracking(int row,int n,char[][] arr,List<List<String>>res){
+        if(row==n){
+        List<String>list=new ArrayList<>();
+        for(int i=0;i<n;i++){
+            list.add(new String(arr[i]));
+        }
+        res.add(list);
+        return ;
+        }
+        for(int col=0;col<n;col++){
+            if(isSafe(row,col,arr,n)){
+                arr[row][col]='Q';
+                backtracking(row+1,n,arr,res);
+                arr[row][col]='.';
+            }
+        }
+  
+
+    }
+    public List<List<String>> solveNQueens(int n) {
+        List<List<String>>list=new ArrayList<>();
+        char chess[][]=new char[n][n];
+        for(char[] ch:chess){
+        Arrays.fill(ch,'.');
+        }
+    
+        backtracking(0,n,chess,list);
+        return list;
+       // return ;
+    }
+    public boolean isSafe(int row,int col,char[][]chess,int n){
+        //horizantal
+       /* for(int i=0;i<n;i++){
+                if(chess[row][i]=='Q'){
+                    return false;
+                } 
+        }*/
+        //vertical check
+        for (int i = row - 1; i >= 0; i--) {
+    if (chess[i][col] == 'Q') return false;
+}
+        //left diagoanl check
+       for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+    if (chess[i][j] == 'Q') return false;
+}
+        //right diagonal check
+        for (int i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++) {
+    if (chess[i][j] == 'Q') return false;
+}
+                
+                return true;
+    }
+}
